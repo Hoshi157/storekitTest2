65ee940706bbc1b3a2d5021c2d0880cd88cc81f0
hoshi.fukuyama <debsen17xxx@icloud.com>
2020-07-22 14:31:26 +0900

    Initial Commit

---
+++ storekitTest2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  storekitTest2
+//
+//  Created by 福山帆士 on 2020/07/22.
+//  Copyright © 2020 福山帆士. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
